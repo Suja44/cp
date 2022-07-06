af03b2bc95fefd131015cd1d2eb78f06d00b9bbe
Suja44 <97326187+Suja44@users.noreply.github.com>
2022-07-06 19:42:34 +0530

    Create numcodes.cpp

---
+++ numcodes.cpp
@@ -0,0 +1,35 @@
+
+int power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b & 1) {
+			res = (res * a) % mod;
+		}
+		a = (a * a) % mod;
+		b /= 2;
+	}
+	return res;
+}
+
+vi primefac(int n){
+	vi v;
+	for(int i=2;i*i<=n;i++){
+		while(n%i==0){
+			v.pb(i);
+			n/=i;
+		}
+     
+	}
+	if(n>1) v.pb(n);
+
+	return v;
+}
+
+bool isprime(int n){
+for(int i=2;i*i<=n;i++){
+   if(n%i==0){
+   return false;
+   }
+  }
+ return true;
+} 
